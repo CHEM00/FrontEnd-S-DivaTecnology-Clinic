26514e67078ed8cdcae1dd1ed521eae56d83ce38
chem00 <zs19017404@estudiantes.uv.mx>
2025-11-25 13:21:43 -0600

    Complementando todas las vistas, refactorizando y ejecutnado responsive

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: true
+  },
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    server: {
+      allowedHosts: ['printless-patentably-india.ngrok-free.dev'],
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3001',
+          changeOrigin: true,
+          secure: false,
+        }
+      }
+    }
   }
 });
